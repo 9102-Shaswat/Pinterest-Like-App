0a1b2c1f85ea8c146283a126f27b3db19b9acbb9
shaswat <shaswat.sherpur@gmail.com>
2023-06-23 22:44:21 +0530

    Routes added

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,28 @@
 import express from 'express'
 import dotenv from 'dotenv'
+dotenv.config()
+
 import cors from 'cors'
 import logger,{morganMiddleware} from './logger'
 import cookieParser from 'cookie-parser'
 import authRoutes from './routes/auth'
+import postRoutes from './routes/post'
+import commentRoutes from './routes/comment'
+import { connectDB } from './utils/db.utils'
 
-dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 8080
+
+connectDB()
 
 app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 app.use(morganMiddleware)
 app.use('/auth', authRoutes)
+app.use('/post', postRoutes)
+app.use('/comment', commentRoutes)
 
 app.get('/', (req, res) =>{
     res.send(`Server is running `)
